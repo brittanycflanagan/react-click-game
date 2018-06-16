a5ced84b1e4f7a8f7d6381ac0926a224a71b6d8f
Brittany Flanagan - Backdrop Express <brittanycturner@gmail.com>
2018-06-16 15:51:55 -0700

    getting close

--- click-game/src/App.js
+++ click-game/src/App.js
@@ -8,45 +8,65 @@ import puppies from "./puppies.json";
 import "./App.css";
 
 let clickedItems = [];
+let scoreVar = 0;
+let topScore = 0;
 
 class App extends Component {
   // Setting this.state.friends to the friends json array
   state = {
-    puppies
+    puppies,
   };
 
   playGame = id => {
    if (!clickedItems.includes(id)) {
     clickedItems.push(id);
-    console.log(clickedItems);
-  //  randomizePics();
+    console.log("UNSHUFFLED:"+puppies);
+    this.shuffleArray(puppies);
+    console.log("SHUFFLED"+puppies);
+    this.setState(puppies);  
+    scoreVar++;
+    if (scoreVar > topScore) {
+      topScore++;
+    }
+
+ 
    } else {
+    scoreVar = 0;
+    clickedItems = [];
     console.log("LOOSE");
+    this.setState(puppies);
    }
    
   };
 
- // randomizePics = id => {
-  //  console.log(true);
- // }
+ shuffleArray = array => {
+    let i = array.length - 1;
+    for (; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      const temp = array[i];
+      array[i] = array[j];
+      array[j] = temp;
+    }
+    return array;
+  }
+
+
 
   // Map over this.state.friends and render a FriendCard component for each friend object
   render() {
     return (
       <div>
-        <Nav>Friends List</Nav>
+        <Nav score={scoreVar} topScore={topScore}>Friends List</Nav>
         <Jumbo />
         <Wrapper>
         {this.state.puppies.map(puppies => (
-      
+          
           <Card
             playGame={this.playGame}
             id={puppies.id}
             key={puppies.id}
             name={puppies.name}
             image={puppies.image}
-            occupation={puppies.occupation}
-            location={puppies.location}
           />
         ))}
       </Wrapper>
--- click-game/src/components/Nav/Nav.js
+++ click-game/src/components/Nav/Nav.js
@@ -3,7 +3,9 @@ import "./Nav.css";
 
 
 const Nav = props => (
-<nav className="navbar"><ul><li className="brand"><a href="/">Clicky Game</a></li><li className="">Click an image to begin!</li><li>Score: 0 | Top Score: 0</li></ul></nav>
+<nav className="navbar"><ul><li className="brand"><a href="/">Clicky Game</a></li>
+<li className="">Click an image to begin!</li>
+<li>Score: {props.score} | Top Score: {props.topScore}</li></ul></nav>
     
 );
 
